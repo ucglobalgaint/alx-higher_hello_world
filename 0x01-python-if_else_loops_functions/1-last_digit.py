6f34ae299c40c900d7e379e19d318e76620ef5ce
ucglobalgaint <uchidozie9@gmail.com>
2023-01-03 05:33:58 -0800

    Task1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-             print("{}".format(chr(letter)), end="")
+import random
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+    print(f"Last digit of {number:d} is {digit:d} and is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
