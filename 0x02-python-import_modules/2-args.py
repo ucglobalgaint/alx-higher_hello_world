6201c4adc7584b1f4974989ff58a20ef4e336863
ucglobalgaint <uchidozie9@gmail.com>
2023-01-08 02:06:06 -0800

    Task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    for in range(count):
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
