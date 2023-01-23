fe9993b9c53394f4c6fe686deae53eed2479630b
ucglobalgaint <uchidozie9@gmail.com>
2023-01-23 04:30:01 -0800

    Task4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -21,9 +21,9 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             div = 0
-       except IndexError:
+        except IndexError:
             print("out of range")
             div = 0
-       finally:
+        finally:
             new_list.append(div)
-       return (new_list)
+        return (new_list)
